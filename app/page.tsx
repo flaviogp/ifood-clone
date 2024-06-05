eb42b498a3e9554e7a6238a143774440f9230f97
FlavioGP <flavio.gpinheiro@hotmail.com>
2024-06-05 10:51:06 -0300

    feat: add promo banner to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from './_components/header'
 import Search from './_components/search'
 import CategoryList from './_components/category-list'
+import Image from 'next/image'
 
 
 const Home = () => {
@@ -13,6 +14,18 @@ const Home = () => {
       </div>
       <div className="px-5 pt-6">
         <CategoryList />
+      </div>
+
+      <div className="px-5 pt-6">
+        <Image 
+          src="/promo-banner-pizza.webp" 
+          alt="Até 30% de desconto em pizzas"
+          height={0}
+          width={0}
+          className='w-full h-auto object-contain'
+          sizes='100vw'
+          quality={100}
+        />
       </div>
     </>
   )
