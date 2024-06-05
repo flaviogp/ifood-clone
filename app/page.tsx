6b46d6e2a959da7ddcc1f6d995dc1371d17327bb
FlavioGP <flavio.gpinheiro@hotmail.com>
2024-06-05 10:38:17 -0300

    feat: add category item and list components

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from './_components/header'
 import Search from './_components/search'
+import CategoryList from './_components/category-list'
 
 
 const Home = () => {
@@ -9,6 +10,9 @@ const Home = () => {
       <Header />
       <div className="px-5 pt-6">
         <Search />
+      </div>
+      <div className="px-5 pt-6">
+        <CategoryList />
       </div>
     </>
   )
