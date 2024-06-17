1e40aafe3eed2717c74cc1fb251d796f18dd6d92
FlavioGP <flavio.gpinheiro@hotmail.com>
2024-06-17 09:40:57 -0300

    fix: show delivery time on product page

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,30 @@
-import React from 'react'
-import Header from './_components/header'
-import Search from './_components/search'
-import CategoryList from './_components/category-list'
-import ProductList from './_components/product-list'
-import { Button } from './_components/ui/button'
-import { ChevronRightIcon} from 'lucide-react'
-import { db } from './_lib/prisma'
-import PromoBanner from './_components/promo-banner'
-import RestaurantList from './_components/restaurant-list'
-
+import React from "react";
+import Header from "./_components/header";
+import Search from "./_components/search";
+import CategoryList from "./_components/category-list";
+import ProductList from "./_components/product-list";
+import { Button } from "./_components/ui/button";
+import { ChevronRightIcon } from "lucide-react";
+import { db } from "./_lib/prisma";
+import PromoBanner from "./_components/promo-banner";
+import RestaurantList from "./_components/restaurant-list";
 
 const Home = async () => {
   const products = await db.product.findMany({
-    where:{
-      discountPercentage:{
-        gt: 0
-      }
+    where: {
+      discountPercentage: {
+        gt: 0,
+      },
     },
     take: 10,
-    include:{
+    include: {
       restaurant: {
         select: {
           name: true,
-        }
+        },
       },
-    }
-  })
-
+    },
+  });
 
   return (
     <>
@@ -39,47 +37,48 @@ const Home = async () => {
       </div>
 
       <div className="px-5 pt-6">
-        <PromoBanner 
-        src="/promo-banner-01.webp"
-        alt='até 30% de descontos em pizzas'
+        <PromoBanner
+          src="/promo-banner-01.webp"
+          alt="até 30% de descontos em pizzas"
         />
-
       </div>
 
-      <div className="pt-6 space-y-4">
-        <div className='px-5 flex justify-between items-center'>
-          <h2 className='font-semibold'>
-            Pedidos recomendados
-          </h2>
-          <Button variant='ghost' className='h-fit p-0 text-primary hover:bg-transparent'>
+      <div className="space-y-4 pt-6">
+        <div className="flex items-center justify-between px-5">
+          <h2 className="font-semibold">Pedidos recomendados</h2>
+          <Button
+            variant="ghost"
+            className="h-fit p-0 text-primary hover:bg-transparent"
+          >
             Ver todos
-            <ChevronRightIcon size={16}/>
+            <ChevronRightIcon size={16} />
           </Button>
         </div>
-        <ProductList products={products}/>
+        <ProductList products={products} />
       </div>
 
       <div className="px-5 pt-6">
-        <PromoBanner 
+        <PromoBanner
           src="/promo-banner-02.webp"
-          alt='A partir de R$ 17,90 em lanches' 
+          alt="A partir de R$ 17,90 em lanches"
         />
       </div>
 
       <div className="space-y-4 py-6">
-        <div className='px-5 flex justify-between items-center'>
-          <h2 className='font-semibold'>
-            Restaurantes recomendados
-          </h2>
-          <Button variant='ghost' className='h-fit p-0 text-primary hover:bg-transparent'>
+        <div className="flex items-center justify-between px-5">
+          <h2 className="font-semibold">Restaurantes recomendados</h2>
+          <Button
+            variant="ghost"
+            className="h-fit p-0 text-primary hover:bg-transparent"
+          >
             Ver todos
-            <ChevronRightIcon size={16}/>
+            <ChevronRightIcon size={16} />
           </Button>
         </div>
         <RestaurantList />
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
