9bc29d2e552672e1fc6d2b6dbf24d0bb325b46a9
FlavioGP <flavio.gpinheiro@hotmail.com>
2024-06-18 11:22:19 -0300

    feat: ensure restaurants can be searched for

--- app/_components/search.tsx
+++ app/_components/search.tsx
@@ -1,19 +1,37 @@
-import React from 'react'
-import { Input } from './ui/input'
-import { Button } from './ui/button'
-import { SearchIcon } from 'lucide-react'
+"use client";
 
-type Props = {}
+import React, { useState } from "react";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import { SearchIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
 
-const search = (props: Props) => {
+const Search = () => {
+  const router = useRouter();
+  const [search, setSearch] = useState("");
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+
+  const handleSearchSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!search) return;
+    router.push(`/restaurant?search=${search}`);
+  };
+
   return (
-    <div className="flex gap-2">
-        <Input placeholder='Buscar restaurantes' className='border-none'/>
-        <Button size='icon'>
-            <SearchIcon size={20}/>
-        </Button>
-    </div>
-  )
-}
+    <form className="flex gap-2" onSubmit={(e) => handleSearchSubmit(e)}>
+      <Input
+        placeholder="Buscar restaurantes"
+        className="border-none"
+        onChange={(e) => handleChange(e)}
+      />
+      <Button size="icon" type="submit">
+        <SearchIcon size={20} />
+      </Button>
+    </form>
+  );
+};
 
-export default search
+export default Search;
