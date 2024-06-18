0fd7fc8dceb8961f301f5c2cb8ed89d5518c4a26
FlavioGP <flavio.gpinheiro@hotmail.com>
2024-06-18 10:07:59 -0300

    feat: add homepage link to header logo

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,22 +1,24 @@
-import Image from 'next/image'
-import React from 'react'
-import { Button } from './ui/button'
-import { MenuIcon } from 'lucide-react'
+import Image from "next/image";
+import React from "react";
+import { Button } from "./ui/button";
+import { MenuIcon } from "lucide-react";
+import Link from "next/link";
 
 const Header = () => {
   return (
-    <header className='flex justify-between px-5 pt-6 pb-0'>
+    <header className="flex justify-between px-5 pb-0 pt-6">
+      <Link href="/">
         <Image src="/logo.png" alt="Ifood clone logo" height={30} width={150} />
-        <Button
-            size='icon'
-            variant='outline'
-            className='bg-transparent border-none'
-        > 
-            <MenuIcon />
-        </Button>
-
+      </Link>
+      <Button
+        size="icon"
+        variant="outline"
+        className="border-none bg-transparent"
+      >
+        <MenuIcon />
+      </Button>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
